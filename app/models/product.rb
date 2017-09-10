1e67cfb3628ca54583b67de0fd30be499a1f6a63
“ABSteingit config --global user.email “abs19stein@gmail.com <andrewstein@Andrews-MacBook-Pro-2.local>
2017-09-10 06:52:34 -0500

    additions

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,30 @@ class Product < ApplicationRecord
   def description_list
     description.split(", ")
   end
+
+  def sale_message
+    if discounted? < 200 #this is a reader method of the attribute of price
+      "Discount Item!"
+    else
+      "Everyday Value!"
+    end
+  end
+  
+  def discounted?
+    if price < 200
+      true
+    else
+      false
+    end
+
+  def tax 
+    price * 0.09
+  end
+
+  def total
+    price + tax 
+  end
+
+    
+  end
 end
